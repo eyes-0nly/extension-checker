93d2f3048e312d05b07ee4a011ba0fa69e679eb7
eyes_only <mailz.eyes.only@gmail.com>
2018-02-13 14:52:58 +0300

    Added support for bmp and tiff

--- rename.py
+++ rename.py
@@ -16,18 +16,28 @@ for filename in os.listdir(os.getcwd()):
             binaryfile.seek(0)
             filetype = binaryfile.read(2)
             filetype = binascii.hexlify(filetype)
-            extension = filename[-4:]
+            extension = filename[filename.index('.'):]
         if filetype == b'8950' and extension != '.png':
-            os.rename(filename, filename.replace(filename[-4:], ".png"))
+            os.rename(filename, filename.replace(extension, ".png"))
             print('Extension of {} changed to png'.format(filename))
             e += 1
         elif filetype == b'ffd8' and extension != '.jpg':
-            os.rename(filename, filename.replace(filename[-4:], ".jpg"))
+            os.rename(filename, filename.replace(extension, ".jpg"))
             print('Extension of {} changed to jpg'.format(filename))
             e += 1
         elif filetype == b'4749' and extension != '.gif':
-            os.rename(filename, filename.replace(filename[-4:], ".gif"))
+            os.rename(filename, filename.replace(extension, ".gif"))
             print('Extension of {} changed to gif'.format(filename))
+            e += 1
+        elif filetype == b'424d' and extension != '.bmp':
+            os.rename(filename, filename.replace(extension, ".bmp"))
+            print('Extension of {} changed to bmp'.format(filename))
+            e += 1
+        elif filetype == b'4949' or \
+                filetype == b'4d4d' and extension != '.tif' or \
+                extension != '.tiff':
+            os.rename(filename, filename.replace(extension, ".tiff"))
+            print('Extension of {} changed to tiff'.format(filename))
             e += 1
         i += 1
 print('Done. {} Files. Changed extensions {}'.format(i, e))
